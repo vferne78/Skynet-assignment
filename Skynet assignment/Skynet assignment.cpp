10fb7535963d02a6f33b36ac28284dcc235a9606
fervalen <fervalen@uat.edu>
2018-01-25 13:04:55 -0700

    first checkin

--- Skynet assignment/Skynet assignment.cpp
+++ Skynet assignment/Skynet assignment.cpp
@@ -14,12 +14,11 @@ int main()
 {
 	srand(static_cast<unsigned int>(time(0)));  //seed random number generator
 
-	int secretNumber = rand() % 100 + 1; //random number between 1 and 100
+	int secretNumber = rand() % 100 + 1; //random number between 1 and 64
 	int tries = 0;
 	int guess;
 
 	cout << "\tWelcome to Guess My Number\n\n";
-
 	do
 	{
 		cout << "Enter a guess: ";
@@ -36,10 +35,10 @@ int main()
 		}
 		else
 		{
-			cout << "\nThat's it! You got it in " << tries << "guesses!\n"
+			cout << "\nThat's it! You got it in " << tries << "guesses!\n";
 		}
 
-	}
+	} while (guess != secretNumber);
     return 0;
 }
 
